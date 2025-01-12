1e57b0cdefef032002936441edea6b5bd9ddc13c
santyjL <jlopezsanty1@gmail.com>
2025-01-12 12:14:55 -0600

    se añadió la función crear_grid para mostrar productos en un GridView dinámico y se actualizaron los componentes de productos

---
+++ backend/crear_productos.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from styles import Colores
+
+
+@dataclass
+class Creador_de_productos:
+    """
+    Clase que define un contenedor de productos.
+    """
+    nombre: str
+    imagen: str
+    descripcion: str
+    color: str
+    width: str
+    height: int
+    expand: int
+
+    def elementos_retorno(self):
+        return (self.nombre, self.imagen,
+                self.descripcion, self.color,
+                self.width, self.height, self.expand)
