55cc85310f5ba924f0262763ed1d16ffbfb7fbe8
santyjL <jlopezsanty1@gmail.com>
2025-01-12 11:10:55 -0600

    se añadió la función barra_comprimida para mejorar la navegación y

--- Components/barra_de_navegacion.py
+++ Components/barra_de_navegacion.py
@@ -4,6 +4,28 @@ from styles import Colores
 
 BGFONDO = Colores.AZUL.value
 
+def barra_comprimida(barra_completa: ft.NavigationDrawer):
+    def abrir_barra(e):
+        barra_completa.open = True
+        barra_completa.update()
+
+    barra = ft.Container(
+        ft.Row(
+        controls=[
+            ft.IconButton(icon="menu",
+                          on_click=abrir_barra,
+                          icon_color=Colores.BLANCO.value
+                    ),
+            ],
+            alignment=ft.MainAxisAlignment.START,  # Alineación al inicio
+        ),
+        expand=True,  # ¡Asegúrate de que este contenedor se expanda!
+        width="full",
+        bgcolor=BGFONDO,
+        margin=0
+    )
+    return barra
+
 def item_de_barra(label, icon):
     return ft.ListTile(
         title=ft.Text(label),
