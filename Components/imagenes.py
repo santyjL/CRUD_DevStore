6bfe4c4002d7546e5f6f2c36ce918f95ffce19cc
santyjL <jlopezsanty1@gmail.com>
2025-01-15 11:06:41 -0600

    se actualizaron los estilos de la función imagenes para ajustar el borde, fondo, ajuste de imagen y agregar sombra

--- Components/imagenes.py
+++ Components/imagenes.py
@@ -9,5 +9,13 @@ def imagenes(src, width=None, height=None):
         width=width,
         height=height,
         border_radius=Estilos.BORDER_RADIUS_2.value,
-        border=ft.border.all(width , Colores.NEGRO.value),
-        )
+        border=ft.border.all(1, Colores.NEGRO.value),  # Ajusta el grosor y color del borde
+        bgcolor=Colores.BLANCO.value,  # Fondo del contenedor
+        image_fit=ft.ImageFit.COVER,  # Ajuste de la imagen (puede ser CONTAIN o COVER según lo que necesites)
+        shadow=ft.BoxShadow(
+            spread_radius=2,
+            blur_radius=6,
+            color=ft.colors.BLACK45,
+            offset=ft.Offset(2, 2)
+        ),  # Sombra alrededor del contenedor
+    )
