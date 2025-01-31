acdff6c2843ef588c2f1ce627856aaa8d0d159fa
santyjL <jlopezsanty1@gmail.com>
2025-01-30 20:36:40 -0600

    se añadió la vista del buscador con título, fondo y barra comprimida

--- frontend/buscador.py
+++ frontend/buscador.py
@@ -1,6 +1,14 @@
 import flet as ft
 
+from Components.barra_comprimida import barra_comprimida
+from styles import Colores
+
 
 def buscador_view(page: ft.Page):
+    page.title = "Buscador"
+    page.bgcolor = Colores.BLANCO.value
+    page.scroll = "auto"
+
     # Contenido de la vista
+    page.controls.append(barra_comprimida(page.drawer))
     page.controls.append(ft.Text("Página del Buscador"))
