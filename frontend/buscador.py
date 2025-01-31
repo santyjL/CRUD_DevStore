18434f2f81985e234d70a0a0b189f817c09666a0
santyjL <jlopezsanty1@gmail.com>
2025-01-30 21:43:34 -0600

    se añadió la funcionalidad del buscador para mostrar productos en una cuadrícula

--- frontend/buscador.py
+++ frontend/buscador.py
@@ -1,7 +1,9 @@
 import flet as ft
 
+from backend.productos import todos_los_productos
 from Components.barra_comprimida import barra_comprimida
-from styles import Colores
+from Components.grid import crear_grid
+from styles import Colores, Estilos
 
 
 def buscador_view(page: ft.Page):
@@ -9,6 +11,21 @@ def buscador_view(page: ft.Page):
     page.bgcolor = Colores.BLANCO.value
     page.scroll = "auto"
 
+    productos =ft.Column(
+        controls=[
+            ft.Container(
+                ft.Row(
+                    controls=[
+                        crear_grid(todos_los_productos, max_extent=350)
+                    ],
+                    alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                margin= Estilos.Margin.value,
+                bgcolor=Colores.GRIS.value,
+            )
+        ],
+    )
+
     # Contenido de la vista
     page.controls.append(barra_comprimida(page.drawer))
-    page.controls.append(ft.Text("Página del Buscador"))
+    page.controls.append(productos)
